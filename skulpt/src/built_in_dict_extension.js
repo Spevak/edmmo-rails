b30653aeb165f3f29bd6ad1bbf219837db6e4adb
Chrysolophus <Chrysolophus@Archen-Nest.local>
2014-03-07 17:38:55 -0800

    Added extensions to builtin and builtin_dict. Link, rework, and debug upon more info about JSON handling + behavior.

---
+++ skulpt/src/built_in_dict_extension.js
@@ -0,0 +1,8 @@
+Sk.builtins_extended = {
+    'go' = Sk.builtin.goFunction,
+    'pickup' = Sk.builtin.pickupFunction,
+    'drop' = Sk.builtin.dropFunction,
+    'use' = Sk.builtin.useFunction,
+    'status' = Sk.builtin.statusFunction,
+    'inspect' = Sk.builtin.inspectFunction
+};
