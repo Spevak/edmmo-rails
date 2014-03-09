c1176e2f35e6e040f83fabb2a0441bc6556797a8
Chrysolophus <Chrysolophus@Archen-Nest.local>
2014-03-09 11:48:28 -0700

    Build problems should be fixed, now to build the dummy server

--- skulpt/builtin.js
+++ skulpt/builtin.js
@@ -1027,21 +1027,33 @@ Sk.builtinFiles = undefined;
 
 /* Let's reuse the POST handler from warmup because we're fantastic like that */
 
+/**
+ * @suppress {missingProperties}
+ */
 function goSuccess(response) {
     return new Sk.builtin.nmber(response.err, Sk.builtin.nmber.int$); //Assumes is 0
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function goFailure(response) {
     if (response.err === 1)
-        return Sk.builtin.str("Can’t walk there...");
+        return new Sk.builtin.str("Can’t walk there...");
     if (response.err === 2)
-        return Sk.builtin.str("Immobilized!");
+        return new Sk.builtin.str("Immobilized!");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function pickupSuccess(response) {
-    return response;
+    return new Sk.builtin.nmber(response, Sk.builtin.nmber.int$);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function pickupFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("That item isn't there!");
@@ -1049,19 +1061,31 @@ function pickupFailure(response) {
         return new Sk.builtin.str("You can't access this tile!");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function dropSuccess(response) {
     return new Sk.builtin.nmber(response.err, Sk.builtin.nmber.int$);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function dropFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("You don't have that item!");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function useSuccess(response) {
     return new Sk.builtin.nmber(response.err, Sk.builtin.nmber.int$);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function useFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("You don't have that item!");
@@ -1069,23 +1093,38 @@ function useFailure(response) {
         return new Sk.builtin.str("Erm... I don't think you can do that with that item.");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function statusSuccess(response) {
-    return new Sk.builtin.tuple(response.hp, response.battery, response.facing);
+    return new Sk.builtin.tuple((response.hp, response.battery, response.facing));
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function statusFailure(response) {
     return new Sk.builtin.str("Something terrible has happened to cause this.");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function inspectSuccess(response) {
     return new Sk.builtin.str(response.item);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function inspectFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("I don't have that item");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function tilesSuccess(response) {
     var newStr = "";
     newStr += "Here are the items we found in the specified range:" + '\n';
@@ -1095,6 +1134,9 @@ function tilesSuccess(response) {
     return new Sk.builtin.str(newStr);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function tilesFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("Something terrible has happened to cause this.");
@@ -1102,11 +1144,14 @@ function tilesFailure(response) {
 
 /* ########################################################################## */
 
+/**
+ * @suppress {missingProperties}
+ */
 function json_post_request(page, successFunction, failureFunction, dict) {
     alert('got to ajax call');
-    /*
 
-    $.ajax({
+
+    /*$.ajax({
         type: 'POST',
         url: page,
         data: JSON.stringify(dict),
@@ -1114,22 +1159,34 @@ function json_post_request(page, successFunction, failureFunction, dict) {
         dataType: "json",
         success: successFunction,
         error: failureFunction
-    });
-    */
+    });*/
+    
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.goFunction = function(dir) {
     json_post_request("player/move", goSuccess, goFailure, {'direction': dir});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.pickupFunction = function(x, y, ID) {
     json_post_request("player/pickup", pickupSuccess, pickupFailure, {'x': x, 'y': y, 'itemID': ID});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.dropFunction = function(ID) {
     json_post_request("player/drop", dropSuccess, dropFailure, {'itemID': ID});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.useFunction = function(ID, args) {
     json_post_request("player/use", useSuccess, useFailure, {
                       'itemID': ID,
@@ -1139,6 +1196,9 @@ Sk.builtin.useFunction = function(ID, args) {
 
 /* ########################################################################### */
 
+/**
+ * @suppress {missingProperties}
+ */
 function json_get_request(page, successFunction, failureFunction, dict) {
     alert('got to json get request');
     /* 
@@ -1154,15 +1214,23 @@ function json_get_request(page, successFunction, failureFunction, dict) {
   */
 }
 
-
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.statusFunction = function() {
     json_get_request("player/get", statusSuccess, statusFailure, {});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.inspectFunction = function(ID) {
     json_get_request("player/pickup", inspectSuccess, inspectFailure, {'itemID': ID});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.tilesFunction = function(n) {
     json_get_request("world/tiles", inspectSuccess, inspectFailure, {'n': n});
 }
--- skulpt/ext/builtin_ext.js
+++ skulpt/ext/builtin_ext.js
@@ -1,21 +1,33 @@
 
 /* Let's reuse the POST handler from warmup because we're fantastic like that */
 
+/**
+ * @suppress {missingProperties}
+ */
 function goSuccess(response) {
     return new Sk.builtin.nmber(response.err, Sk.builtin.nmber.int$); //Assumes is 0
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function goFailure(response) {
     if (response.err === 1)
-        return Sk.builtin.str("Can’t walk there...");
+        return new Sk.builtin.str("Can’t walk there...");
     if (response.err === 2)
-        return Sk.builtin.str("Immobilized!");
+        return new Sk.builtin.str("Immobilized!");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function pickupSuccess(response) {
-    return response;
+    return new Sk.builtin.nmber(response, Sk.builtin.nmber.int$);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function pickupFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("That item isn't there!");
@@ -23,19 +35,31 @@ function pickupFailure(response) {
         return new Sk.builtin.str("You can't access this tile!");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function dropSuccess(response) {
     return new Sk.builtin.nmber(response.err, Sk.builtin.nmber.int$);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function dropFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("You don't have that item!");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function useSuccess(response) {
     return new Sk.builtin.nmber(response.err, Sk.builtin.nmber.int$);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function useFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("You don't have that item!");
@@ -43,23 +67,38 @@ function useFailure(response) {
         return new Sk.builtin.str("Erm... I don't think you can do that with that item.");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function statusSuccess(response) {
-    return new Sk.builtin.tuple(response.hp, response.battery, response.facing);
+    return new Sk.builtin.tuple((response.hp, response.battery, response.facing));
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function statusFailure(response) {
     return new Sk.builtin.str("Something terrible has happened to cause this.");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function inspectSuccess(response) {
     return new Sk.builtin.str(response.item);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function inspectFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("I don't have that item");
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function tilesSuccess(response) {
     var newStr = "";
     newStr += "Here are the items we found in the specified range:" + '\n';
@@ -69,6 +108,9 @@ function tilesSuccess(response) {
     return new Sk.builtin.str(newStr);
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 function tilesFailure(response) {
     if (response.err === 1)
         return new Sk.builtin.str("Something terrible has happened to cause this.");
@@ -76,11 +118,14 @@ function tilesFailure(response) {
 
 /* ########################################################################## */
 
+/**
+ * @suppress {missingProperties}
+ */
 function json_post_request(page, successFunction, failureFunction, dict) {
     alert('got to ajax call');
-    /*
 
-    $.ajax({
+
+    /*$.ajax({
         type: 'POST',
         url: page,
         data: JSON.stringify(dict),
@@ -88,22 +133,34 @@ function json_post_request(page, successFunction, failureFunction, dict) {
         dataType: "json",
         success: successFunction,
         error: failureFunction
-    });
-    */
+    });*/
+    
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.goFunction = function(dir) {
     json_post_request("player/move", goSuccess, goFailure, {'direction': dir});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.pickupFunction = function(x, y, ID) {
     json_post_request("player/pickup", pickupSuccess, pickupFailure, {'x': x, 'y': y, 'itemID': ID});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.dropFunction = function(ID) {
     json_post_request("player/drop", dropSuccess, dropFailure, {'itemID': ID});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.useFunction = function(ID, args) {
     json_post_request("player/use", useSuccess, useFailure, {
                       'itemID': ID,
@@ -113,6 +170,9 @@ Sk.builtin.useFunction = function(ID, args) {
 
 /* ########################################################################### */
 
+/**
+ * @suppress {missingProperties}
+ */
 function json_get_request(page, successFunction, failureFunction, dict) {
     alert('got to json get request');
     /* 
@@ -128,15 +188,23 @@ function json_get_request(page, successFunction, failureFunction, dict) {
   */
 }
 
-
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.statusFunction = function() {
     json_get_request("player/get", statusSuccess, statusFailure, {});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.inspectFunction = function(ID) {
     json_get_request("player/pickup", inspectSuccess, inspectFailure, {'itemID': ID});
 }
 
+/**
+ * @suppress {missingProperties}
+ */
 Sk.builtin.tilesFunction = function(n) {
     json_get_request("world/tiles", inspectSuccess, inspectFailure, {'n': n});
 }
