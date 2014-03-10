e42a7159e1b644b10f5323cdafea1a72e97bad00
Chrysolophus <Chrysolophus@Archen-Nest.local>
2014-03-09 21:26:05 -0700

    Added modified World Controller

--- app/controllers/world_controller.rb
+++ app/controllers/world_controller.rb
@@ -9,18 +9,13 @@ class WorldController < ApplicationController
     def tiles
         # Take the user as a parameter, along with password
         myDim = params[:n]
-        returncode = Worlds.tiles(myDim)
-        # Make a JSON for error and successful execution
-        errJSON = {:err => returncode}
-        returnJSON = {:err => $SUCCESS}
-    
-        # We can get away with this because 1 is the only successful error code
-        if returncode == -1
-            render :json => errJSON
-            else
+        if myID.n?
+            returnJSON = {:err => 1}
+            render :json => returnJSON
+            else # This is a placeholder since no access to activeRecords
+            returnJSON = {:err => 0}
             render :json => returnJSON
         end
-    end
 
 
 end
