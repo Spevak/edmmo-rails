bf14d339fa3ac8d9c064cd7ccf265f3d904ac837
Grayson Chao <graysonchao@berkeley.edu>
2014-03-11 21:25:21 -0700

    Implemented character model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  it "returns all logged in users" do
-    @users = (1..5).collect { FactoryGirl.create(:logged_in_user) }
-    logged_in_users = User.logged_in_users
-    logged_in_users.count.should eq(5)
+  describe ".logged_in_users" do
+    it "returns all logged in users" do
+      @users = (1..5).collect { FactoryGirl.create(:logged_in_user) }
+      logged_in_users = User.logged_in_users
+      logged_in_users.count.should eq(5)
+    end
   end
 end
