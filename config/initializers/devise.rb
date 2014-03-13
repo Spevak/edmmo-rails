a425866ddcaa5c89d90cf64cd7db98d96b25d8a1
Grayson Chao <graysonchao@berkeley.edu>
2014-03-12 18:11:14 -0700

    Log out everyone automatically on init

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -222,7 +222,7 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -256,4 +256,8 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  #
+
+  # ==> Make sure that logged_in is consistent between server runs
+  User.all.each do |u| u.logged_in = false ; u.save! end
 end
