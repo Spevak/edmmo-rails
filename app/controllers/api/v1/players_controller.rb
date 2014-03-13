1b92bd81eb6f2b455a0cb74f424ac44210f5e694
Grayson Chao <graysonchao@berkeley.edu>
2014-03-13 15:02:36 -0700

    Fixed brokenness screwing up rspec in players controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::PlayersController < Api::V1::BaseController
 
   def move_to(direction)
     direction = request[:direction]
-    user_location = User.
   end
 
   def pick_up(x, y, item_id)
