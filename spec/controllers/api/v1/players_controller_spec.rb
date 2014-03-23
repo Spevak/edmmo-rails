1a0ba280bee060c4159d4b160b02db0b7a43682a
szhou8813 <zealot.x@hotmail.com>
2014-03-23 13:51:03 -0700

    player controller test "drop" added

--- spec/controllers/api/v1/players_controller_spec.rb
+++ spec/controllers/api/v1/players_controller_spec.rb
@@ -15,10 +15,10 @@ describe Api::V1::PlayersController do
             json = {direction: 'east'}
             post :move, json
             it "moves the player and returns error code 0 (success)" do
-                @character.tile.x.should == 1
-                @character.tile.y.should == 0
-                Tile.tile_at(0, 0).character.should == nil
-                JSON.parse(response.body)["err"].should == 0
+                @character.tile.x.should eql 1
+                @character.tile.y.should eql 0
+                Tile.tile_at(0, 0).character.should eql nil
+                JSON.parse(response.body)["err"].should eql 0
             end
         end
         context "invalid move" do
@@ -34,9 +34,9 @@ describe Api::V1::PlayersController do
             json = {direction: 'south'}
             post :move, json
             it "does not move player and returns error code 1" do
-                @character.tile.x.should == 0
-                @character.tile.y.should == 0
-                JSON.parse(response.body)["err"].should == 1
+                @character.tile.x.should eql 0
+                @character.tile.y.should eql 0
+                JSON.parse(response.body)["err"].should eql 1
             end
         end
     end
@@ -57,9 +57,9 @@ describe Api::V1::PlayersController do
             json = {x: 0, y: 0, itemID: @item.id}
             post :pickup, json
             it "picks up an item and put it in the player's hands. Return error code 0" do
-                @character.item.id.should == @item.id
-                @tile.item.should == nil
-                JSON.parse(response.body)["err"].should == 0
+                @character.item.id.should eql @item.id
+                @tile.item.should eql nil
+                JSON.parse(response.body)["err"].should eql 0
             end
         context "item not there" do
             @user = FactoryGirl.create(:user)
@@ -75,8 +75,8 @@ describe Api::V1::PlayersController do
             json = {x: 0, y: 0, itemID: @item.id}
             post :pickup, json
             it "returns error code 1" do
-                @character.item.should == nil
-                JSON.parse(response.body)["err"].should == 1
+                @character.item.should eql nil
+                JSON.parse(response.body)["err"].should eql 1
             end
         end
         context "tile not accessible" do
@@ -96,8 +96,8 @@ describe Api::V1::PlayersController do
             json = {x: 1, y: 0, itemID: @item.id}
             post :pickup, json
             it "returns error code 2" do
-                @character.item.should == nil
-                JSON.parse(response.body)["err"].should == 2
+                @character.item.should eql nil
+                JSON.parse(response.body)["err"].should eql 2
             end
         end
         context "hands occupied" do
@@ -117,20 +117,58 @@ describe Api::V1::PlayersController do
             json = {x: 0, y: 0, itemID: @item.id}
             post :pickup, json
             it "returns error code 3" do
-                @character.item.id.should == @item2.id
-                @tile.item.id.should == @item.id
-                JS.parse(response.body)["err"].should == 3
+                @character.item.id.should eql @item2.id
+                @tile.item.id.should eql @item.id
+                JSON.parse(response.body)["err"].should eql 3
             end
         end
     end
 
     describe "POST #drop" do
         context "valid drop" do
+            @user = FactoryGirl.create(:user)
+            sign_in @user
+            @tiles = (1..side_length ** 2).collect { FactoryGirl.create(:tile) }
+            @character = FactoryGirl.create(:character)
+            @item = FactoryGirl.create(:item)
+            @tile = Tile.tile_at(0, 0)
+            @tile.character = @character
+            @tile.save
+            @character.user = @user
+            @character.item = @item
+            @character.save
+            json = {x: 0, y: 0, itemID: @item.id}
+            post :drop, json
             it "drops an item and returns error code 0" do
+                @tile.item.id.should eql  @item.id
+                @character.item.should eql nil
+                JSON.parse(response.body)["err"].should eql 0
             end
         end
         context "does not have such item" do
             it "returns error code 1" do
+            end
+        end
+        context "tile occupied by another item" do
+            @user = FactoryGirl.create(:user)
+            sign_in @user
+            @tiles = (1..side_length ** 2).collect { FactoryGirl.create(:tile) }
+            @character = FactoryGirl.create(:character)
+            @item = FactoryGirl.create(:item)
+            @tile = Tile.tile_at(0, 0)
+            @tile.character = @character
+            @tile.item = @item
+            @tile.save
+            @item2 = FactoryGirl.create(:item)
+            @character.user = @user
+            @character.item = @item2
+            @character.save
+            json = {x: 0, y: 0, itemID: @item2.id}
+            post :drop, json
+            it "returns error code 2" do
+                @tile.item.id.should eql @item.id
+                @character.item.id.should eql @item2.id
+                JSON.parse(response.body)["err"].should eql 1
             end
         end
     end
