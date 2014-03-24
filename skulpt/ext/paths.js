17cebf479e3b385f38893e00c690065389b60bea
Kevin Spevak <jimmy@jimmy-VirtualBox.(none)>
2014-03-24 00:23:46 -0700

    Set up capybara tests (added dependencies in tests/test_helper.rb and added features dir under spec).  Also added option argument to rake built_skulpt task: running 'rake build_skulpt testing=true' will result in an interpreter build using the test API paths.  If this argument is left out it will build with the paths specified in skulpt/ext/paths.js (currently v1 api)

--- skulpt/ext/paths.js
+++ skulpt/ext/paths.js
@@ -1,16 +1,4 @@
-//TEST BACKEND
-/*
-var MOVE_PATH = "api/test/player/move";
-var PICKUP_PATH = "api/test/player/pickup";
-var DROP_PATH = "api/test/player/drop";
-var USE_PATH = "api/test/player/use";
-var STATUS_PATH = "api/test/player/status";
-var INSPECT_PATH = "api/test/player/inspect";
-var CHARACTERS_PATH = "api/test/player/characters";
-var TILES_PATH = "api/test/world/tiles";
-*/
-//PRODUCTION BACKEND
-
+//PRODUCTION BACKEND PATHS
 
 var MOVE_PATH = "api/v1/player/move";
 var PICKUP_PATH = "api/v1/player/pickup";
---
+++ spec/features/test_feature_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "Hello Capybara" do
+  it "works" do
+    puts "Hello World!"
+  end
+end
