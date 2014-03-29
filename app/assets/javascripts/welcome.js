e3033477385827bb0304f135ee99e672d71e240f
Grayson Chao <graysonchao@berkeley.edu>
2014-03-29 15:20:05 -0700

    Fixed tile coordinate system to expose y positive-up update function

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,37 +1,45 @@
 $(document).ready(function() {
 	var cellWidth = 12,
-			cellHeight = cellWidth,
-			textWidth = 12,
-			textHeight = textWidth,
-			cellBgColor = 'white',
-			cellFgColor = 'red',
-			cellHighlightBgColor = 'yellow',
-			cellHighlightFgColor = 'black',
-			layers = [],
-			cells = [],
-			offset = Math.floor(mapData.n / 2); //add to coordinates s.t. player is at 0, 0
+	cellHeight = cellWidth,
+	textWidth = 12,
+	textHeight = textWidth,
+	cellBgColor = 'white',
+	cellFgColor = 'red',
+	cellHighlightBgColor = 'yellow',
+	cellHighlightFgColor = 'black',
+	layers = [],
+	cells = [],
+	offset = Math.floor(mapData.n / 2); //add to coordinates s.t. player is at 0, 0
 
+	// Holds all the cells - mapData.n**2 of them - needed to buffer
+	// half a map away on each side of the map.
+	// stage: a Kinetic.Stage
+	// cells: a square 2d array of cells w/ side length mapData.n ** 2
+	var MapBuffer = function(stage, cells) {
+		this.stage = stage;
+		this.cells = cells;
+	}
+
+	// options: {
+	//	direction: '[north|south|east|west]' (required)
+	//	distance: [0..floor(mapWidth/2)]. default = 1
+	// }
+	// returns false if unsuccessful.
+	// otherwise, returns an array of cells to populate the map.
+	MapBuffer.prototype.scroll = function(options) {
+		var distance, direction;
+		if (!options['direction']) {
+			return false;
+		} 
+		distance = options['distance'] || 1;
+		direction = options['direction'];
+	}
 
 	// Always returns a string which is unique for any pair (x, y)
 	var hashCellPair = function(x, y) {
 		x -= offset;
 		y -= offset;
 		return (x * mapData.n) + y;
-	}
-
-	// Abstraction for dealing with map cells, which are composed of
-	// several Kinetic objects.
-	var getCellById = function (id) {
-		var cell = stage.find("#" + id)[0];
-		var cellOuter = cell.getChildren()[0];
-		var cellInner = cell.getChildren()[1];
-		return {
-			'outer': cellOuter,
-			'inner': cellInner,
-			'alert': function() {
-				this.outer.setAttr('fill', cellHighlightBgColor)
-				this.inner.setAttr('fill', cellHighlightFgColor)
-			}}
 	}
 
 	// Set up the DOM
@@ -69,7 +77,7 @@ $(document).ready(function() {
 		for (var j = 0; j < mapData.n; j++) {
 			// Just trust me
 			var x = j,
-					y = i;
+			y = i;
 
 			var cell = new Kinetic.Group({
 				x: (x * cellWidth),
@@ -90,6 +98,7 @@ $(document).ready(function() {
 				fill: cellFgColor,
 				width: textWidth,
 				height: textHeight,
+				fontFamily:'Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New, monospace',
 				x: 0,
 				y: 0
 			});
@@ -99,6 +108,44 @@ $(document).ready(function() {
 
 			cells[i].push(cell);
 		}
+	}
+
+	// Abstraction for dealing with map cells, which are composed of
+	// several Kinetic objects.
+	// outer = a Kinetic.Node object
+	// inner = a Kinetic.Node object
+	// layer = a Kinetic.Layer object
+	var Cell = function(outer, inner, layer) {
+		this.outer = outer;
+		this.inner = inner;
+		this.layer = layer;
+	}
+
+	Cell.prototype.alert = function() {
+		this.outer.setAttr('fill', cellHighlightBgColor)
+		this.inner.setAttr('fill', cellHighlightFgColor)
+		this.layer.draw();
+
+		// Fucking js
+		var that = this;
+		window.setTimeout(function() {
+			that.outer.setAttr('fill', cellBgColor)
+			that.inner.setAttr('fill', cellFgColor)
+			that.layer.draw();
+		}, 2000);
+	}
+
+	Cell.prototype.update = function(outer, inner) {
+		this.outer.setAttrs(outer);
+		this.inner.setAttrs(inner);
+		this.layer.draw();
+	};
+
+	var getCellById = function (id) {
+		var cell = stage.find("#" + id)[0];
+		var cellOuter = cell.getChildren()[0];
+		var cellInner = cell.getChildren()[1];
+		return new Cell(cellOuter, cellInner, fgLayer);
 	}
 
 	for (var i = 0; i < cells.length; i++) {
@@ -113,10 +160,10 @@ $(document).ready(function() {
 	// im pretty sure our app isnt called "window"
 	// so i should change this pretty soon -grayson
 	window.updateBotQuest = function(x, y) {
-			var cell = getCellById(hashCellPair(x, y));
-			var newCellContents = mapData.tileAt(x, y);
-			cell.inner.setText(newCellContents);
-			cell.alert();
-			fgLayer.draw(); // must call to update the <canvas>.
+		var cell = getCellById(hashCellPair(x, (mapData.n - y)));
+		var newCellContents = mapData.tileAt(x, y);
+		cell.inner.setText(newCellContents);
+		fgLayer.draw(); // must call to update the <canvas>.
+		cell.alert();
 	}
 });
