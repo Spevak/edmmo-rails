789336f34e9bf7e3daccbf04864879d9e691b87d
szhou8813 <zealot.x@hotmail.com>
2014-04-05 13:13:34 -0700

    welcome controller back

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+    unless user_signed_in? or Rails.env.development? then
+      redirect_to new_user_session_path
+      end
+     if current_user.character == nil then
+      character = Character.create(name: "robot9000", health: 100, battery: 100, facing: 0, planet: 1)
+      character.setTile(Tile.first)
+      character.save!
+      current_user.character = character
+      current_user.save!
+    end
+  end
+end
