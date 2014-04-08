9a394f683418965c0d3ed6a082e9b5844bfdd242
Kevin Spevak <jimmy@jimmy-VirtualBox.(none)>
2014-04-08 16:31:47 -0700

    more progress on map editor gui.  Commiting before attempting to manually merge jeromie and michel's changes to dev

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,4 +4,11 @@ class MapController < ApplicationController
       raise ActionController::RoutingError.new('Not Found')
     end
   end
+  def load
+    x = request[:x]
+    y = request[:y]
+    filename = "config/map/" + x.to_s + "-" + y.to_s + ".json"
+    file = open(filename)
+    render :json => file.read
+  end
 end
