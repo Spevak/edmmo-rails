4a585e0d2ac6a1cfffc0063a2a84fd7ad77a8a0e
Kevin Spevak <jimmy@jimmy-VirtualBox.(none)>
2014-04-09 15:44:32 -0700

    fixed model issue on heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,19 @@
 class WelcomeController < ApplicationController
   def index
-    if not (user_signed_in? or Rails.env.development?) then
-      redirect_to new_user_session_path
-    else
-      if current_user.character == nil then
-        character = Character.create(name: "robot9000", health: 100, battery: 100, facing: 0, planet: 1)
-        character.setTile(Tile.tile_at(0, 0))
-        character.save!
-        current_user.character = character
-        current_user.save!
-      elsif current_user.character.tile == nil then
-        #Change this so we spawn in a meaningful location 
-        current_user.character.setTile(Tile.tile_at(0,0))
+    unless Rails.env.development? then 
+      if not user_signed_in? then
+        redirect_to new_user_session_path
+      else
+        if current_user.character == nil then
+          character = Character.create(name: "robot9000", health: 100, battery: 100, facing: 0, planet: 1)
+          character.setTile(Tile.tile_at(0, 0))
+          character.save!
+          current_user.character = character
+          current_user.save!
+        elsif current_user.character.tile == nil then
+          #Change this so we spawn in a meaningful location 
+          current_user.character.setTile(Tile.tile_at(0,0))
+        end
       end
     end
   end
